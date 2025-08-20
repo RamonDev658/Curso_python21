e2d9bbcfd254e6da755a0a2122dc11e06ca0d726
RamonDev658 <ramondev658@gmail.com>
2025-08-20 06:17:13 -0300

    Interpolação básica de strings

---
+++ aula15.py
@@ -0,0 +1,9 @@
+## Interpolação básica de strings
+
+### S - string, d e i - int, f - float , x e X - Hexadecimal(ABCDEF0123...)
+
+nome = 'Luiz'
+preco = 1000.9343433
+variavel = '%s, o preço é %.2f' % (nome, preco)
+print(variavel)
+print('O hexadecimal de %d é %06x' % (10589, 10589))
