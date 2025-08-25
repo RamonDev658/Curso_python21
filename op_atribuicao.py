b28966a416c9249b221773c979e341c5d70f5feb
Ramon Silva <ramondev658@gmail.com>
2025-08-25 18:54:55 -0300

    Operadores de atribuição

---
+++ op_atribuicao.py
@@ -0,0 +1,24 @@
+## Operadores de atribuição
+
+"""
+=, igual(atribuição)
++= , atribuição por soma
+-=, atribuição  por subtração
+*= , multiplicação
+/=, divisão
+//=,divisão inteira
+**= ,potenciação
+%= modulo
+
+"""
+
+
+
+contador = 0
+
+
+while contador <= 10:
+    print(contador)
+    contador += 1
+    
+print('Acabou')    
